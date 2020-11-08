fad068ac8bf615a247a982ae6250c7ffb7ea1b1c
devchea <rchea817@gmail.com>
2020-11-07 20:27:25 -0800

    "updates"

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { tether, Container, Heading } from '@triframe/designer'
+import { tether, Container, Heading, Button } from '@triframe/designer'
 
-export const Dashboard = tether(function*(){
+export const Dashboard = tether(function*({ redirect }){
+    const logout = () => {
+        localStorage.clear()
+        redirect("/")
+    }
+
     return (
         <Container>
             <Heading>Menu</Heading>
+            <Button
+                onPress={logout}
+            >
+                Logout
+            </Button>
         </Container>
     )
 })
