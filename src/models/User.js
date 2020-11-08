c9bbbf4537a65d93104669ea25ef108ab3221945
josieyao <yao.josephine@yahoo.com>
2020-11-08 11:46:56 -0800

    created cart state

--- src/models/User.js
+++ src/models/User.js
@@ -1,51 +1,53 @@
-import { Resource } from '@triframe/core'
-import { include, Model, string, session, stream, hasMany } from '@triframe/scribe'
-import { hash, compare } from 'bcrypt'
+import { Resource } from "@triframe/core";
+import {
+  include,
+  Model,
+  string,
+  session,
+  stream,
+  hasMany,
+} from "@triframe/scribe";
+import { hash, compare } from "bcrypt";
 
 export class User extends Resource {
-    @include(Model)
+  @include(Model)
+  @string
+  username = "";
 
-    @string 
-    username = ""
+  @string
+  passwordDigest = "";
 
-    @string
-    passwordDigest = ""
-
-    static async register (username, password) {
-        let existingUsersWithTheSameName = await User.where({ username: username })
-        if(existingUsersWithTheSameName.length > 0){
-            throw Error('A user with this username already exists')
-        }
-        let passwordDigest = await hash(password, 10)
-        return User.create({ username: username, passwordDigest: passwordDigest })
+  static async register(username, password) {
+    let existingUsersWithTheSameName = await User.where({ username: username });
+    if (existingUsersWithTheSameName.length > 0) {
+      throw Error("A user with this username already exists");
     }
+    let passwordDigest = await hash(password, 10);
+    return User.create({ username: username, passwordDigest: passwordDigest });
+  }
 
-    @session
-    static async login(session, username, password){
+  @session
+  static async login(session, username, password) {
+    let [user] = await User.where({ username });
 
-        let [ user ] = await User.where({ username })
-        console.log('user', user);
-
-        if(user == undefined){
-            throw Error('Username or Password are incorrect')
-        }
-        if(!await compare(password, user.passwordDigest)){
-            throw Error('Username or Password are incorrect')
-        }
-        session.loggedInUserId = user.id
-        return true
+    if (user == undefined) {
+      throw Error("Username or Password are incorrect");
     }
-
-    @session
-    @stream
-    static *current(session){
-        return (
-            session.loggedInUserId !== null
-                ? yield User.read(session.loggedInUserId)
-                : null
-        )
+    if (!(await compare(password, user.passwordDigest))) {
+      throw Error("Username or Password are incorrect");
     }
+    session.loggedInUserId = user.id;
+    return true;
+  }
 
-    @hasMany
-    orders=[]
+  @session
+  @stream
+  static *current(session) {
+    return session.loggedInUserId !== null
+      ? yield User.read(session.loggedInUserId)
+      : null;
+  }
+
+  @hasMany
+  orders = [];
 }
